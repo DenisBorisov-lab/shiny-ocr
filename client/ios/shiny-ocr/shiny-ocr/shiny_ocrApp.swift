05038fd00c4cf67c96d6a070ab6c7486c24f669c
Igor Buzykin <lexemz@gmail.com>
2022-09-19 23:12:06 +0300

    initial ios project

---
+++ client/ios/shiny-ocr/shiny-ocr/shiny_ocrApp.swift
@@ -0,0 +1,17 @@
+//
+//  shiny_ocrApp.swift
+//  shiny-ocr
+//
+//  Created by Igor Buzykin on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct shiny_ocrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
