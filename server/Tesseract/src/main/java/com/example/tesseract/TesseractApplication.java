32769047bc6acf1e0bb100981d990bc0e6bb1d25
Denis Borisov <mai.borisov.denis@gmail.com>
2022-09-19 23:10:41 +0300

    add inital server

---
+++ server/Tesseract/src/main/java/com/example/tesseract/TesseractApplication.java
@@ -0,0 +1,14 @@
+package com.example.tesseract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesseractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesseractApplication.class, args);
+    }
+
+}
+
---
+++ server/Tesseract/src/test/java/com/example/tesseract/TesseractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tesseract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesseractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
