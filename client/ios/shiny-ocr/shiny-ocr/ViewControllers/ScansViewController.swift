0a4173b41d6561d08c2bfb3e102d9f09fbe12a31
Igor Buzykin <lexemz@gmail.com>
2022-09-20 13:55:10 +0300

    Add a language selection menu

--- client/ios/shiny-ocr/shiny-ocr/ViewControllers/ScansViewController.swift
+++ client/ios/shiny-ocr/shiny-ocr/ViewControllers/ScansViewController.swift
@@ -10,18 +10,33 @@ import UIKit
 class ScansViewController: UIViewController {
   @IBOutlet var tableView: UITableView!
 
-  let scans = RecivedScan.mock()
+  private let scans = RecivedScan.mock()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.delegate = self
     tableView.dataSource = self
     tableView.rowHeight = UITableView.automaticDimension
-    
     tableView.register(
       ScanTableViewCell.instanceFromNib(),
       forCellReuseIdentifier: ScanTableViewCell.id
     )
+
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Русский")
+    navigationItem.rightBarButtonItem?.menu = generateChangeLanguageMenu()
+  }
+
+  private func generateChangeLanguageMenu() -> UIMenu {
+    var actions: [UIAction] = []
+    ["Руссикй", "English"].forEach { element in
+      let action = UIAction(
+        title: element,
+        state: element == "Руссикй" ? .on : .off,
+        handler: { _ in }
+      )
+      actions.append(action)
+    }
+    return UIMenu(title: "Язык Сканирования", children: actions)
   }
 }
 
@@ -29,13 +44,13 @@ extension ScansViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     scans.count
   }
-  
+
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(
       withIdentifier: ScanTableViewCell.id,
       for: indexPath
     ) as? ScanTableViewCell else { return UITableViewCell() }
-    
+
     cell.configure(scan: scans[indexPath.row])
     cell.accessoryType = .disclosureIndicator
     return cell
