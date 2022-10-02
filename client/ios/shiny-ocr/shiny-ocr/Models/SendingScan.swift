2f8a1448a491fc7f96b3a992a994758fd66d2459
Igor Buzykin <lexemz@gmail.com>
2022-10-02 19:06:36 +0300

    Update SendingScan model

--- client/ios/shiny-ocr/shiny-ocr/Models/SendingScan.swift
+++ client/ios/shiny-ocr/shiny-ocr/Models/SendingScan.swift
@@ -10,12 +10,21 @@ import Foundation
 // ws://192.168.1.2:8080/scanner-server
 
 struct SendingScan: Encodable {
-  let language: Language
+  let language: OCRLanguage
   let format: String
   let image: Data
   let userId: UUID
 }
 
-enum Language: String, CaseIterable, Encodable {
+enum OCRLanguage: String, CaseIterable, Encodable {
   case rus, eng
+  
+  var introduction: String {
+    switch self {
+    case .rus:
+      return "Русский"
+    case .eng:
+      return "Английский"
+    }
+  }
 }
