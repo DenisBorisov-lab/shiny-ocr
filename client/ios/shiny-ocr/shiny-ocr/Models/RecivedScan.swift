fabf538970ef67cf6f6588b7a82b2945df1847a7
Igor Buzykin <lexemz@gmail.com>
2022-10-02 19:44:48 +0300

    Update RecivedScan model

--- client/ios/shiny-ocr/shiny-ocr/Models/RecivedScan.swift
+++ client/ios/shiny-ocr/shiny-ocr/Models/RecivedScan.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct RecivedScan: Decodable {
-  let id: Int
+  let uuid: UUID
   let scanText: String
   let scanDate: Date
 }
@@ -16,9 +16,9 @@ struct RecivedScan: Decodable {
 extension RecivedScan {
   static func mock() -> [Self] {
     [
-      .init(id: 1233, scanText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu faucibus nibh. In volutpat tellus nec est imperdiet, at venenatis justo commodo. Integer sed turpis ac quam laoreet rutrum. In hac habitasse platea dictumst. Quisque ultricies vitae purus sed auctor. Suspendisse nunc tortor, cursus id purus at, dictum volutpat ipsum. Nunc diam purus, pretium id congue vitae, tempus dictum ante.", scanDate: Date()),
-      .init(id: 551, scanText: "SomeScanText", scanDate: Date()),
-      .init(id: 4921, scanText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu faucibus nibh.", scanDate: Date())
+      .init(uuid: UUID(), scanText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu faucibus nibh. In volutpat tellus nec est imperdiet, at venenatis justo commodo. Integer sed turpis ac quam laoreet rutrum. In hac habitasse platea dictumst. Quisque ultricies vitae purus sed auctor. Suspendisse nunc tortor, cursus id purus at, dictum volutpat ipsum. Nunc diam purus, pretium id congue vitae, tempus dictum ante.", scanDate: Date()),
+      .init(uuid: UUID(), scanText: "SomeScanText", scanDate: Date()),
+      .init(uuid: UUID(), scanText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu faucibus nibh.", scanDate: Date())
     ]
   }
 }
