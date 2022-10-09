1371b8e8d1d830845872620c92112f9cdca3948c
Igor Buzykin <lexemz@gmail.com>
2022-10-10 01:21:34 +0300

    Update ScannerManager

--- client/ios/shiny-ocr/shiny-ocr/Managers /ScannerManager.swift
+++ client/ios/shiny-ocr/shiny-ocr/Managers /ScannerManager.swift
@@ -8,44 +8,26 @@
 import Foundation
 
 protocol ScannerManagerDelegate: AnyObject {
-  func lockScanActions()
-  func unlockScanActions()
   func scannerManager(_ manager: ScannerManager, didReceiveScan: RecivedScan)
 }
 
 class ScannerManager {
-  private let webSocket: WebSocketProvider = NativeWebSocket(url: URL(string: "ws://localhost:8080/scanner-server")!)
+  private let socket = SocketIO(url: URL(string: "ws://localhost:8081")!)
   weak var delegate: ScannerManagerDelegate?
+  var isConnected = false
   
   func connectToTesseractServer() {
-    webSocket.delegate = self
-    webSocket.connect()
+    socket.delegate = self
+//    socket.connect()
   }
     
   func scan(_ scan: SendingScan) {
     print("Sending: \(scan)")
-    webSocket.send(Data())
   }
 }
 
-extension ScannerManager: WebSocketDelegate {
-  func webSocketDidConnect() {
-    print("Socket did connect")
-    delegate?.unlockScanActions()
-  }
-    
-  func webSocketDidDisconnect() {
-    print("Socket did disconnect")
-    delegate?.lockScanActions()
-  }
-    
-  func webSocket(didReceiveString string: String) {
-    print("WARN: web socket reveice string! Expected type: Data")
-  }
-    
-  func webSocket(didReceiveData data: Data) {
-    print(data)
-//    guard let receivedScan = try? JSONDecoder().decode(RecivedScan.self, from: data) else { return }
-//    delegate?.scannerManager(self, didReceiveScan: receivedScan)
+extension ScannerManager: SocketIODelegate {
+  func receiveData(_ data: Data) {
+    //
   }
 }
